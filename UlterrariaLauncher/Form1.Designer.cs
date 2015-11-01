758b3c265d956f082302bd7e5616238e08173be6
Batchfink <jamesmmizen@gmail.com>
2015-11-01 11:11:48 +0000

    Initial commit

---
+++ UlterrariaLauncher/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace UlterrariaLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progLbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progBar = new System.Windows.Forms.ProgressBar();
+            this.versionBox = new System.Windows.Forms.ComboBox();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.installBtn = new System.Windows.Forms.Button();
+            this.controlPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.playBtn = new System.Windows.Forms.Button();
+            this.pathBox = new System.Windows.Forms.MaskedTextBox();
+            this.fdb = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.controlPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 250);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progLbl
+            // 
+            this.progLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progLbl.Location = new System.Drawing.Point(3, 254);
+            this.progLbl.Name = "progLbl";
+            this.progLbl.Size = new System.Drawing.Size(297, 19);
+            this.progLbl.TabIndex = 3;
+            this.progLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 231);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // progBar
+            // 
+            this.progBar.Location = new System.Drawing.Point(3, 162);
+            this.progBar.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.progBar.Name = "progBar";
+            this.progBar.Size = new System.Drawing.Size(294, 36);
+            this.progBar.TabIndex = 7;
+            // 
+            // versionBox
+            // 
+            this.versionBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.versionBox.FormattingEnabled = true;
+            this.versionBox.Location = new System.Drawing.Point(3, 108);
+            this.versionBox.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.versionBox.Name = "versionBox";
+            this.versionBox.Size = new System.Drawing.Size(294, 21);
+            this.versionBox.TabIndex = 6;
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(218, 52);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseBtn.TabIndex = 5;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // installBtn
+            // 
+            this.installBtn.Location = new System.Drawing.Point(3, 3);
+            this.installBtn.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.installBtn.Name = "installBtn";
+            this.installBtn.Size = new System.Drawing.Size(145, 43);
+            this.installBtn.TabIndex = 0;
+            this.installBtn.Text = "Patch version";
+            this.installBtn.UseVisualStyleBackColor = true;
+            this.installBtn.Click += new System.EventHandler(this.installBtn_Click);
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.Controls.Add(this.installBtn);
+            this.controlPanel.Controls.Add(this.playBtn);
+            this.controlPanel.Controls.Add(this.pathBox);
+            this.controlPanel.Controls.Add(this.browseBtn);
+            this.controlPanel.Controls.Add(this.versionBox);
+            this.controlPanel.Controls.Add(this.progBar);
+            this.controlPanel.Controls.Add(this.textBox1);
+            this.controlPanel.Controls.Add(this.progLbl);
+            this.controlPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.controlPanel.Location = new System.Drawing.Point(0, 256);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(300, 224);
+            this.controlPanel.TabIndex = 1;
+            // 
+            // playBtn
+            // 
+            this.playBtn.Location = new System.Drawing.Point(148, 3);
+            this.playBtn.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(147, 43);
+            this.playBtn.TabIndex = 9;
+            this.playBtn.Text = "Play";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(3, 52);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(209, 20);
+            this.pathBox.TabIndex = 8;
+            this.pathBox.Text = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Terraria";
+            this.pathBox.TextChanged += new System.EventHandler(this.pathBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 480);
+            this.Controls.Add(this.controlPanel);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Ulterraria Launcher";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.controlPanel.ResumeLayout(false);
+            this.controlPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label progLbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progBar;
+        private System.Windows.Forms.ComboBox versionBox;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button installBtn;
+        private System.Windows.Forms.FlowLayoutPanel controlPanel;
+        private System.Windows.Forms.MaskedTextBox pathBox;
+        private System.Windows.Forms.FolderBrowserDialog fdb;
+        private System.Windows.Forms.Button playBtn;
+    }
+}
+
---
+++ UlterrariaLauncher/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+using System.Diagnostics;
+
+namespace UlterrariaLauncher
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument doc = new XmlDocument();
+        //         number, link
+        Dictionary<string, string> versions = new Dictionary<string, string>();
+
+        //path to terraria folder
+        string path = Properties.Settings.Default.path;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            firstTimeSetUp();
+            string xml;
+            //get xml from dropbox
+            using (WebClient client = new WebClient())
+            {
+                xml = client.DownloadString("https://www.dropbox.com/s/3pfym0u1kznsqr0/versions.xml?dl=1");
+            }
+
+            doc.LoadXml(xml);
+            //select nodes to get to version and link
+            XmlNodeList nl = doc.SelectNodes("versions");
+            XmlNode root = nl[0];
+
+            foreach (XmlNode xnode in root.ChildNodes)
+            {
+                //add version number to combobox
+                versionBox.Items.Add(xnode.ChildNodes.Item(0).InnerText);
+                //add version number and link to dictionary
+                versions.Add(xnode.ChildNodes.Item(0).InnerText, xnode.ChildNodes.Item(1).InnerText);
+            }
+            //set combobox to select top version
+            versionBox.SelectedIndex = 0;
+        }
+
+        //EVENT HANDLERS
+
+        //play button
+        private void installBtn_Click(object sender, EventArgs e)
+        {
+            
+            //select correct link according to combobox selection
+            string link = versions[Convert.ToString(versionBox.SelectedItem)];
+            
+            try
+            {
+                //delete zip and extract
+                File.Delete(path + @"\Content\Launcher\dl.zip");
+                //Array.ForEach(Directory.GetFiles(path + @"\Content\Launcher\extract"), File.Delete);
+                string extractPath = path + @"\Content\Launcher\extract";
+                foreach (string p in Directory.GetFiles(extractPath))
+                {
+                    File.Delete(p);
+                }
+                string[] subFiles = Directory.GetDirectories(extractPath);
+                foreach (string dir in subFiles)
+                {
+                    foreach (string p in Directory.GetFiles(dir))
+                    {
+                        File.Delete(p);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                progLbl.Text = "ow";
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            try
+            {
+                progLbl.Text = "Downloading...";
+                //download zip from dropbox
+                using (var client = new WebClient())
+                {
+                    //set up event handlers
+                    client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                    client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+
+                    client.DownloadFileAsync(new Uri(link), path + @"\Content\Launcher\dl.zip");
+                }
+            }
+            catch (Exception ex)
+            {
+                progLbl.Text = "ow";
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            //Async download bar yay
+            progBar.Maximum = (int)e.TotalBytesToReceive / 100;
+            progBar.Value = (int)e.BytesReceived / 100;
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            //extract zip to 'extract' folder
+            try
+            {
+                progLbl.Text = "Unzipping...";
+                ZipFile.ExtractToDirectory(path + @"\Content\Launcher\dl.zip", path + @"\Content\Launcher\extract");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Unzip Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            copyFiles();
+        }
+
+
+        //other methods
+        private void copyFiles()
+        {
+            try
+            {
+                progLbl.Text = "Copying files...";
+                //copy terraria.exe
+                File.Copy(path + @"\Content\Launcher\extract\Terraria.exe", path + @"\Terraria.exe", true);
+
+                //copy dlls
+                File.Copy(path + @"\Content\Launcher\extract\CSteamworks.dll", path + @"\CSteamworks.dll", true);
+                File.Copy(path + @"\Content\Launcher\extract\Ionic.Zip.dll", path + @"\Ionic.Zip.dll", true);
+                File.Copy(path + @"\Content\Launcher\extract\Newtonsoft.Json.dll", path + @"\Newtonsoft.Json.dll", true);
+                File.Copy(path + @"\Content\Launcher\extract\Steamworks.NET.dll", path + @"\Steamworks.NET.dll", true);
+
+                //copy ulterraria content
+                //ew visual basic
+                new Microsoft.VisualBasic.Devices.Computer().FileSystem.CopyDirectory(path + @"\Content\Launcher\extract\Ulterraria", path + @"\Content\Ulterraria", Microsoft.VisualBasic.FileIO.UIOption.OnlyErrorDialogs);
+                progLbl.Text = "Done!";
+                progBar.Value = 0;
+
+                
+                
+            }
+            catch (Exception ex)
+            {
+                progLbl.Text = "ow";
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void firstTimeSetUp()
+        {
+            if (!Directory.Exists(path + @"\Content\Launcher"))
+            {
+                Directory.CreateDirectory(path + @"\Content\Launcher");
+                Directory.CreateDirectory(path + @"\Content\Launcher\extract");
+            }
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            if (fdb.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = fdb.SelectedPath;
+            }
+
+            pathBox.Text = path;
+        }
+
+        private void pathBox_TextChanged(object sender, EventArgs e)
+        {
+            path = pathBox.Text;
+            Properties.Settings.Default.path = pathBox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string exePath = path + @"\Terraria.exe";
+                Process.Start(exePath);
+            }
+            catch (Exception ex)
+            {
+                progLbl.Text = "ow";
+                MessageBox.Show(ex.Message, "copy Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
